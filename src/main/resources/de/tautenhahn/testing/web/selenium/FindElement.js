376355404996f033ce6342a51cbf8408cbada310
Dr. Thomas Tautenhahn <t-tautenhahn@gmx.de>
2019-07-30 07:46:02 +0200

    added positional functions

--- src/main/resources/de/tautenhahn/testing/web/selenium/FindElement.js
+++ src/main/resources/de/tautenhahn/testing/web/selenium/FindElement.js
@@ -1,5 +1,5 @@
 function describedAs(e, text){
-   return matches(e.innerText, text) || _l(e, text)
+   return matches(e.innerText, text) || _l(e, text) || matches(e.getAttribute('title'), text)
 }
 function matches(v, text){return v===text||RegExp('^'+text+'$').test(v)}
 function _list_(root, elem){return [... document.querySelector(root).querySelectorAll(elem)]}
@@ -11,4 +11,7 @@ function _desc_(e) {
    return {id: _id_(e), left: rect.left + hoff, right:rect.right+ hoff, bottom:rect.bottom+voff, top:rect.top+voff}
 }
 function _id_(e){if(!e.id){e.id ='wg'+Math.random().toString(36).substr(2, 9)} return e.id}
+function sameLineAs(e, top, bottom) {r=_desc_(e); return top<=r.bottom && bottom>=r.top}
+function before(e, top, bottom, left, right) {r=_desc_(e); return top>r.bottom || bottom>=r.top && left>r.right}
+function after(e, top, bottom, left, right) {r=_desc_(e); return bottom<r.top || top top<=r.bottom && right<r.left}
 
